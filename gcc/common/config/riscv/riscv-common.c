b2fbae44db2f4e4973fe7b98f070d26b5434ff7e
Kito Cheng <kito.cheng@sifive.com>
2020-04-10 17:20:18 +0800

    RISC-V: Update march parser
     - The arch string rule has changed in latest spec, it introduced new
       multi-letter extension prefix with 'h' and 'z', and drop `sx`. also
       adjust parsing order for 's' and 'x'.
    
    gcc/ChangeLog
    
    	* riscv-common.c (parse_sv_or_non_std_ext): Rename to
    	parse_multiletter_ext.
    	(parse_multiletter_ext): Add parsing `h` and `z`, drop `sx`,
    	adjust parsing order for 's' and 'x'.
    
    gcc/testsuite/ChangeLog
    
    	* gcc.target/riscv/arch-3.c: Adjust option.
    	* gcc.target/riscv/arch-5.c: New.
    	* gcc.target/riscv/attribute-9.c: Adjust option and test
    	condition.

--- gcc/common/config/riscv/riscv-common.c
+++ gcc/common/config/riscv/riscv-common.c
@@ -73,8 +73,8 @@ private:
 
   const char *parse_std_ext (const char *);
 
-  const char *parse_sv_or_non_std_ext (const char *, const char *,
-				       const char *);
+  const char *parse_multiletter_ext (const char *, const char *,
+				     const char *);
 
 public:
   ~riscv_subset_list ();
@@ -364,7 +364,7 @@ riscv_subset_list::parse_std_ext (const char *p)
     {
       char subset[2] = {0, 0};
 
-      if (*p == 'x' || *p == 's')
+      if (*p == 'x' || *p == 's' || *p == 'h' || *p == 'z')
 	break;
 
       if (*p == '_')
@@ -451,20 +451,20 @@ riscv_is_supported_pulp_ext (const char *ext)
   return false;
 }
 
-/* Parsing function for non-standard and supervisor extensions.
+/* Parsing function for multi-letter extensions.
 
    Return Value:
      Points to the end of extensions.
 
    Arguments:
      `p`: Current parsing position.
-     `ext_type`: What kind of extensions, 'x', 's' or 'sx'.
+     `ext_type`: What kind of extensions, 's', 'h', 'z' or 'x'.
      `ext_type_str`: Full name for kind of extension.  */
 
 const char *
-riscv_subset_list::parse_sv_or_non_std_ext (const char *p,
-					    const char *ext_type,
-					    const char *ext_type_str)
+riscv_subset_list::parse_multiletter_ext (const char *p,
+					  const char *ext_type,
+					  const char *ext_type_str)
 {
   unsigned major_version = 0;
   unsigned minor_version = 0;
@@ -480,11 +480,6 @@ riscv_subset_list::parse_sv_or_non_std_ext (const char *p,
 	}
 
       if (strncmp (p, ext_type, ext_type_len) != 0)
-	break;
-
-      /* It's non-standard supervisor extension if it prefix with sx.  */
-      if ((ext_type[0] == 's') && (ext_type_len == 1)
-	  && (*(p + 1) == 'x'))
 	break;
 
       char *subset = xstrdup (p);
@@ -555,21 +550,26 @@ riscv_subset_list::parse (const char *arch, location_t loc)
   if (p == NULL)
     goto fail;
 
-  /* Parsing non-standard extension.  */
-  p = subset_list->parse_sv_or_non_std_ext (p, "x", "non-standard extension");
-
-  if (p == NULL)
-    goto fail;
-
   /* Parsing supervisor extension.  */
-  p = subset_list->parse_sv_or_non_std_ext (p, "s", "supervisor extension");
+  p = subset_list->parse_multiletter_ext (p, "s", "supervisor extension");
 
   if (p == NULL)
     goto fail;
 
-  /* Parsing non-standard supervisor extension.  */
-  p = subset_list->parse_sv_or_non_std_ext
-    (p, "sx", "non-standard supervisor extension");
+  /* Parsing hypervisor extension.  */
+  p = subset_list->parse_multiletter_ext (p, "h", "hypervisor extension");
+
+  if (p == NULL)
+    goto fail;
+
+  /* Parsing sub-extensions.  */
+  p = subset_list->parse_multiletter_ext (p, "z", "sub-extension");
+
+  if (p == NULL)
+    goto fail;
+
+  /* Parsing non-standard extension.  */
+  p = subset_list->parse_multiletter_ext (p, "x", "non-standard extension");
 
   if (p == NULL)
     goto fail;
